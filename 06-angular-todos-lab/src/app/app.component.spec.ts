2bdf0b709f4d837f1d221d74b1bd3149dc08614a
Borislava <bobiivanova00@gmail.com>
2022-04-20 18:19:40 +0300

    add app component

--- 06-angular-todos-lab/src/app/app.component.spec.ts
+++ 06-angular-todos-lab/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '06-angular-todo'`, () => {
+  it(`should have as title 'mmt-2022-05-angular-todos-lab'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('06-angular-todo');
+    expect(app.title).toEqual('mmt-2022-05-angular-todos-lab');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('06-angular-todo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mmt-2022-05-angular-todos-lab app is running!');
   });
 });
--- 06-angular-todos-lab/src/app/app.component.ts
+++ 06-angular-todos-lab/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { TodoService } from './todo.service';
+import { Component, OnInit } from '@angular/core';
+import { Todo, TodoStatus } from './todo.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'td-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular Todos Demo';
+export class AppComponent implements OnInit {
+  title = 'Angular TODOs Demo';
+  todos: Todo[] = [];
+  constructor(private todoService: TodoService){}
+  
+  ngOnInit(): void {
+    this.todos = this.todoService.getAllTodos();
+  }
+
+  getTodoStatusString(todoStatus: TodoStatus){
+    return TodoStatus[todoStatus];
+  }
+
+
 }
